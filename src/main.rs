a9fa8a00c20af3f4314f046fa117d9b5f843e14f
atboxshop <atboxshop@gmail.com>
2023-02-14 10:41:05 -0500

    123

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::prelude::*;
 use panic_halt as _;
+
+
 
 #[arduino_hal::entry]
 fn main()->!
 {
     let dp = arduino_hal::Peripherals::take().unwrap();
-
+    
     let pins = arduino_hal::pins!(dp);
-
+    
+    let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
+    
     let mut led = pins.d13.into_output();
 
     loop {
         led.toggle();
-        arduino_hal::delay_ms(5000);
+        arduino_hal::delay_ms(500);
+        ufmt::uwriteln!(&mut serial, "Hello From Arduino\r").void_unwrap();
     }
 }
