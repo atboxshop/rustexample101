01b1079fe75917593173d0e251b7a5e9a749c98b
atboxshop <atboxshop@gmail.com>
2023-03-03 05:45:17 -0500

    123

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 #![no_main]
 
 use arduino_hal::prelude::*;
-use arduino_hal::simple_pwm::*;
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -13,31 +12,12 @@ fn main()->!
     let pins = arduino_hal::pins!(dp);
     
     let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
-    ufmt::uwriteln!(&mut serial, "Hello From Arduino\r").void_unwrap();
+    
+    let mut led = pins.d9.into_output();
 
-    //Chip arduno Uno 328p run at 8Mhz (default in hardware), divider prescaler 64 --> 8,000Khz/64 = 125Khz
-    //Chip arduino uno 328p has 3 time counter as the datasheet
-    let timer0 = Timer0Pwm::new(dp.TC0, Prescaler::Prescale64);
-    let timer1 = Timer1Pwm::new(dp.TC1, Prescaler::Prescale64);
-    let mut timer2 = Timer2Pwm::new(dp.TC2, Prescaler::Prescale64);
-
-    // Use digital pin 5,6 is connected to a LED and a resistor in series for only timer0
-    let mut pwm_led = pins.d5.into_output().into_pwm(&timer0);
-    pwm_led.enable();// Enable it to run
-    // Use digital pin 9,10 is connected to a LED and a resistor in series for only timer1
-    let mut pwm_led1 = pins.d9.into_output().into_pwm(&timer1);
-    pwm_led1.enable();
-    // Use digital pin 3,11 is connected to a LED and a resistor in series for only timer1
-    let mut pwm_led2 = pins.d3.into_output().into_pwm(&mut timer2);
-    pwm_led2.enable();
-
-    loop 
-    {
-        for x in (0..=255).chain((0..=254).rev()) {
-            pwm_led.set_duty(x);
-            pwm_led1.set_duty(x);
-            pwm_led2.set_duty(x);
-            arduino_hal::delay_ms(5);
-        }
+    loop {
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        ufmt::uwriteln!(&mut serial, "Hello From Arduino\r").void_unwrap();
     }
 }
