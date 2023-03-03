caa4a2b0e830dfc7c92a784841124cd21ec16655
atboxshop <atboxshop@gmail.com>
2023-03-03 18:49:21 -0500

    change code

--- bk/blinky_v2.rs
+++ bk/blinky_v2.rs
@@ -5,8 +5,6 @@ use arduino_hal::prelude::*;
 use arduino_hal::*;
 use panic_halt as _;
 
-
-
 #[arduino_hal::entry]
 fn main()->!
 {
@@ -14,11 +12,15 @@ fn main()->!
     
     let pins = arduino_hal::pins!(dp);
     
-    let mut serial = default_serial!(dp, pins, 115200);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
+    
+    let mut led = pins.d9.into_output();
 
-    loop 
-    {
-        delay_ms(1000);
+    loop {
+        led.set_high();
+        delay_ms(500);
         ufmt::uwriteln!(&mut serial, "Hello From Arduino\r").void_unwrap();
+        led.set_low();
+        delay_ms(500);
     }
 }
